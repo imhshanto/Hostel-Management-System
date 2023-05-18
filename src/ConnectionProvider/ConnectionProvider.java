430b88a1db426cb29af2c1b651287e02f9729ce2
Hasibul Islam Shanto <116863537+imhshanto@users.noreply.github.com>
2023-05-18 17:54:55 +0600

    Add files via upload

---
+++ src/ConnectionProvider/ConnectionProvider.java
@@ -0,0 +1,22 @@
+package ConnectionProvider;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+    public static Connection getCon() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hostel", "root", "987654321");
+            return con;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
